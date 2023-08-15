331bc72ba46a82a6035e9dcd2519c424afdfbebd
Aryan <polygot882@gmail.com>
2023-08-15 19:30:48 +0530

    a1

--- Dynamic_Programming/Longest_Common_Subsequence.cpp
+++ Dynamic_Programming/Longest_Common_Subsequence.cpp
@@ -12,9 +12,9 @@ Longest common subsequence is - "ac" (length = 2)
 
 (subsequence - can be discontinuous also )
 
-Logic - since we can select any common elements of both the strings , we can select distonious elements also
+Logic - since we can select any common elements of both the strings , we can select discontnous elements also
 
-Now , we have a choice to pick elements to form an lcs or not (choice and decisions)  and optimal soln (longest) is asked .
+Now , we have a choice to pick elements to form an lcs or not (choice and decisions)  and optimal soln (longest) is asked .(DP quesn)
 so , we find the choice diag . and BC 
 
 // Choice diag  -
@@ -35,6 +35,10 @@ ALways remebeer , recursive funcn. works like magic if written correctly (here r
 */
 
 
+int dp[1002][1002] ;
+
+
+
 int LCS (string x , string y , int len_x , int len_y){
 
 // Base condition
@@ -44,32 +48,43 @@ if(len_x==0 || len_y==0 ){
 return 0;
 
 }
+// Memoized code 
+
+if(dp[len_x][len_y]!=-1){
+    return dp[len_x][len_y] ;
+}
 
 // Choice diag .
 
 
 // condition 1 : if last char of both strings are same , it will always be included in our subsequence
 if( x[len_x -1 ] == y[len_y -1 ]){
-    return 1 + LCS (x , y , len_x -1 , len_y - 1) ;
+    return dp[len_x][len_y] = 1 + LCS (x , y , len_x -1 , len_y - 1) ;
 
 }
 
 // condition 2 : if last char are NOT same , take 2 possibilities that any other char of theirs can match .
 // So now , find maximum among LCS length from selecting 1 char less  part of 1 string  and whole part of other string .
-else if(x[len_x -1] != y[len_y-1]){
 
-    return max (LCS (x , y , len_x , len_y -1)  , LCS (x , y , len_x -1 , len_y)) ;
+else {
+
+    return dp[len_x][len_y] = max (LCS (x , y , len_x , len_y -1)  , LCS (x , y , len_x -1 , len_y)) ;
 }
 
 
 }
+
+
+// Top Down approach - 
+
+
 
 
 int main (){
 
   string x = "abcfgh" ;
   string y = "acgtt";
-
+  memset(dp , -1 , sizeof dp) ;
   int LCS_len = LCS(x , y , x.size() , y.size()) ; // correct answer =  3 .
 
   cout << LCS_len << endl ;
