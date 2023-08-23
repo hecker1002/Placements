26973814cb62aac5693d5abdf3f4ca19f0509e56
Aryan <polygot882@gmail.com>
2023-08-24 00:40:16 +0530

    added some code for Linked list

--- Linked_List/Reverse_A_LInked_List.cpp
+++ Linked_List/Reverse_A_LInked_List.cpp
@@ -1,21 +1,168 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+// Iterative and Recursive soln both .
+// In LL , node* curr OR node* head is default way to access nodes 
 
 // Creating  a Linked  List (custom data type ==> collection of node s )
 
-struct Node {
+class node {
+
+public :
 
   int data ;
-  struct Node* ptr ;
+  node* next ;
 
-} ;
+  node(int val){
+
+    data= val ;
+    next = NULL ;
+  }
+};
+
+// No need to change the head , so pass by value (node* head )
+
+// iterative soln .
+
+node* reverse_LL (node* head ){
+
+  if(head==NULL){
+    return NULL ;
+  }
+
+// iterator  node 
+  node* curr = head ;
+
+
+stack<node*> st ;
+
+while(curr->next!=NULL){
+  st.push(curr) ;
+  curr = curr->next ;
+}
+
+node* ans = curr ;
+
+while(!st.empty()){
+
+  node* prev = st.top() ;
+
+ curr->next = prev ;
+ curr = curr->next ;
+  st.pop() ;
+}
+
+curr->next = NULL ;
+
+return ans  ;
+
+}
+
+
+
+// recursive soln to reverse a LL , 
+
+node* reverse_LL_recursion (node* head){
+
+  // BC 
+if(head==NULL){
+  return NULL ;
+}
+
+  if(head->next==NULL ){
+
+    return head ;
+  }
+  // hypothesis - it can reverse LL for  any head 
+  // here by hypothesis , we mean that the recursive function reverses the LL and returns to us , the head of such a reversed LL .
+
+  // Induction 
+
+  node* curr = head->next ;
+
+  node* head1 = reverse_LL_recursion(curr) ; // making input smaller
+
+
+// re-defining the connection of reversed LL with node just after head in LL , 
+//it should be connected(head->next->next = is next of node after head node) with the old head node . and old head node's next = NULL.
+ head->next->next  = head ;
+  head->next = NULL ;
+
+return head1 ;
+
+
+}
+void insert_LL (node* &head , int val ){
+
+  node* curr = head ;
+
+  node* add = new node(val) ;
+
+
+if(head== NULL){
+
+  head = add ;
+  return ;
+}
+  while(curr->next!=NULL){
+
+    curr = curr->next ;
+  }
+
+  curr->next = add ;
+
+}
+
+
+void display(node* head){
+
+  node* curr = head ;
+if(head==NULL){
+    return ;
+
+  }
+
+  while(curr != NULL){
+    cout << curr->data << " " ;
+    curr = curr->next ;
+  }
+
+cout << endl ;
+}
+
 
 
 
 
 int main(){
   
+  node* head = NULL ;
+
+  insert_LL(head , 3) ;
+  insert_LL(head , 4) ;
+  insert_LL(head , 5) ;
+
+
+// before reversal 
+display(head) ;
+
+node* new_head  = reverse_LL(head) ;
+
+// after reversal 
+
+display(new_head) ;
+
+// after revrsal from recursion 
+
+node* head2 = NULL ;
+
+  insert_LL(head2 , 3) ;
+  insert_LL(head2 , 4) ;
+  insert_LL(head2 , 5);
+
+
+node* new_head1  = reverse_LL_recursion(head2) ;
+display(new_head1) ;
 
     return 0 ;
 }
---
+++ Linked_List/Traversal_in_LL.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+
+/*
+
+Travel in a Linked List .
+
+Linked List is a linear dsat structure which stores the elements in a linear way and is like a dynamic-sized array .
+building block of LL are - Nodes 
+
+Nodes - are memory spaces where data and address of the next node is stored . and these nodes are connected in a chain 
+(in non-contiguous way) and that makes a linked list .
+
+make a linked list by declaring a custom data type (node) whose data members are data and address of the next node and use representatio
+of a node (like node curr) to access any node of a linked list and  and last nodes of a LL -> points to NULL (nothing) to symbolize
+the end of a LL .
+
+by convention , first node of a LL = head node .
+
+Node x = is a repreentation of the node 
+
+*/
+
+// Making the Node (custom data type )
+
+
+/*
+
+Constructors in C++ - these functions / methods are called automatically when n instance of the object is created .
+AND VERY VERY IMP. " CONSTRUCTORS IN C++ HAVE SAME NAME AS CLASS NAME .  and constructors do NOT need to be called seperately .
+
+*/
+
+class node {
+
+public :
+
+// data stored in current node 
+ int data ;
+// address of the next node  
+  node* next ;
+
+// constructor so when a node is initialized in memory , it makes all these aprts ready-made 
+
+ node(int val ){
+    data = val ;
+    next = NULL ;
+ }
+
+} ;
+
+
+
+
+
+
+// we use node* &head to pass head by reference (and not by value) since we need to update head too if LL is empty .
+
+void insert_In_LL( node* &head , int data  ){
+
+node* Add = new node(data) ;
+
+node* curr = head ;
+
+// Corner Case 
+if(head==NULL){
+    
+ head = Add ;
+ return ;
+
+}
+
+while(curr->next!=NULL){
+
+curr = curr->next ;
+}
+curr->next = Add ;
+
+}
+
+
+
+
+//Traverse in  a LL (in a linear way )
+
+// we pass head by value (and not by reference ) , since we are NOT changing any head or any other nde of the LL .
+
+void traverse_LL (node* head ){
+
+
+node* curr = head ;
+
+// edge case 
+if(head==NULL){
+    return ;
+}
+
+// travel in a LL till we reach the last node .
+
+while(curr!=NULL){
+    cout << curr->data << " " ;
+    curr = curr->next ;
+}
+cout << endl ; 
+
+
+}
+
+
+// main function 
+
+int main(){
+
+// make an empty linked list .
+node* head  = NULL ;
+
+  insert_In_LL(head , 3) ;
+  insert_In_LL(head , 4) ;
+  insert_In_LL(head , 5) ;
+
+  traverse_LL(head) ;
+
+    return 0 ;
+}
+
+/*
+
+While dealing with nodes in a Linked Lisat , always use pointers , ( node* name_of_node )
+
+*/
